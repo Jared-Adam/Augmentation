8a059cfdd3db33734bb1354c94b0a842e0b7d792
Jared Adam <79530043+Jared-Adam-PSU@users.noreply.github.com>
2023-10-20 15:13:02 -0400

    not normal

--- augmenttion.r
+++ augmenttion.r
@@ -15,7 +15,7 @@ beans_test
 
 timing <- c("total", "damaged")
 
-# test of difference ####
+# test of difference ###
 test <- beans_test %>% 
   group_by(leaf_plot) %>% 
   mutate(timing = timing) %>% 
@@ -35,7 +35,7 @@ test_new
 diff(test$total_area[1:2])
 diff(test$total_area[3:4])
 
-# full data set ####
+# full data set ###
 beans$leaf_plot <- as.factor(beans$leaf_plot)
 # now to do this to the whole data set (beans)
 beans_new <- beans %>% 
@@ -64,4 +64,73 @@ beans_sum$leaf_plot[beans_sum$leaf_plot=='Aug_1200'] <-'1200_Aug'
 beans_sum$leaf_plot[beans_sum$leaf_plot=='Ctl_1200'] <-'1200_Ctl'
 beans_sum$leaf_plot[beans_sum$leaf_plot=='Dep_1200'] <-'1200_Dep'
 
+# split the names to have a new column of rep and trt 
+beans_split <- beans_sum
+beans_split$trt <- rownames(beans_split)
+beans_split$trt <- str_split_fixed(beans_split$trt, "_",2)
+
+beans_split[c('plot', 'trt')] <- str_split_fixed(beans_split$leaf_plot, '_',2)
+beans_final <- beans_split %>% 
+ select(plot, trt, dmg_diff) %>% 
+  rename(damage = dmg_diff) %>% 
+  arrange(plot) %>% 
+  mutate(plot = as.numeric(plot)) %>% 
+  mutate(trt = as.factor(trt))
+as_tibble(beans_final)
+
+unique(beans_final$trt)
+
+# damage is not normal 
+kruskal.test(damage ~ trt, data = beans_final)
+
+# vis of data with summary stats 
+group_by(beans_final, trt) %>% 
+  summarise(
+    count = n(), 
+    mean = mean(damage),
+    sd = sd(damage), 
+    median = median(damage), 
+      IQR = IQR(damage)
+  )
+library(ggpubr)
+ggboxplot(beans_final, x = 'trt', y = 'damage')
+
+
+##
+
+?shapiro.test
+shapiro.test(beans_final$damage)
+
+# beans_final$damage <- log10(beans_final$damage)
+
+qqnorm(beans_final$damage)
+
+hist(beans_final$damage)
+
+?aov
+model <- aov(damage ~ trt, beans_final)
+summary(model)
+shapiro.test(model$residuals)
+
+
+##
+
 #now I need to go back and get the total area
+# the first number is total, so I want to every other value, starting with 1 
+beans_total_test <- beans_test %>% 
+  select(leaf_plot, total_area) 
+# going to extract every odd row number
+rows <- nrow(beans_total_test)
+odd_rows <- seq_len(rows) %% 2
+beans_test_odd <- beans_total_test[odd_rows == 1, ]
+
+# all data odd extraction
+
+
+
+
+
+
+
+
+
