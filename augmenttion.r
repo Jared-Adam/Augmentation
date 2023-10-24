c6500d8e24b49235d00a059ea7c82222260fee6e
Jared Adam <79530043+Jared-Adam-PSU@users.noreply.github.com>
2023-10-24 07:53:39 -0400

    took out >15000 and made boxplot

--- augmenttion.r
+++ augmenttion.r
@@ -8,6 +8,7 @@ beans
 beans <- beans %>% 
   mutate(leaf_plot = gsub(".jpg", "", leaf_plot))
 
+# test code #### 
 # time to do math 
 # I just want a few rows to mess with first
 beans_test <- beans[1:10,]
@@ -22,6 +23,7 @@ test <- beans_test %>%
   select(leaf_plot, total_area, timing)
 test
 
+### omit anything above 10000
 #get the difference of each two columns
 test_new <- test %>% 
   group_by(leaf_plot) %>% 
@@ -35,7 +37,13 @@ test_new
 diff(test$total_area[1:2])
 diff(test$total_area[3:4])
 
-# full data set ###
+# test summing plots  
+test_sum <- test_new 
+test_sum <- test_sum %>% 
+  mutate(leaf_plot = gsub("_[0-9]*$","", leaf_plot)) #removes all trailing numbers starting at the underscore
+test_here <- aggregate(dmg_diff ~ leaf_plot , data = test_sum, FUN = sum)
+
+# full data set ####
 beans$leaf_plot <- as.factor(beans$leaf_plot)
 # now to do this to the whole data set (beans)
 beans_new <- beans %>% 
@@ -48,12 +56,6 @@ beans_new <- beans %>%
   
 beans_new 
 
-# test summing plots  
-test_sum <- test_new 
-test_sum <- test_sum %>% 
-  mutate(leaf_plot = gsub("_[0-9]*$","", leaf_plot)) #removes all trailing numbers starting at the underscore
-test_here <- aggregate(dmg_diff ~ leaf_plot , data = test_sum, FUN = sum)
-
 # difference by plot 
 beans_sum <- beans_new %>% 
   mutate(leaf_plot = gsub("_[0-9]*$", "", leaf_plot))
@@ -65,9 +67,9 @@ beans_sum$leaf_plot[beans_sum$leaf_plot=='Ctl_1200'] <-'1200_Ctl'
 beans_sum$leaf_plot[beans_sum$leaf_plot=='Dep_1200'] <-'1200_Dep'
 
 # split the names to have a new column of rep and trt 
-beans_split <- beans_sum
-beans_split$trt <- rownames(beans_split)
-beans_split$trt <- str_split_fixed(beans_split$trt, "_",2)
+# beans_split <- beans_sum
+# beans_split$trt <- rownames(beans_split)
+# beans_split$trt <- str_split_fixed(beans_split$trt, "_",2)
 
 beans_split[c('plot', 'trt')] <- str_split_fixed(beans_split$leaf_plot, '_',2)
 beans_final <- beans_split %>% 
@@ -75,13 +77,35 @@ beans_final <- beans_split %>%
   rename(damage = dmg_diff) %>% 
   arrange(plot) %>% 
   mutate(plot = as.numeric(plot)) %>% 
-  mutate(trt = as.factor(trt))
+  mutate(trt = as.factor(trt)) %>%
+  filter(damage < 15000)
 as_tibble(beans_final)
 
 unique(beans_final$trt)
 
-# damage is not normal 
+# normality 
+?shapiro.test
+shapiro.test(beans_final$damage)
+
+# beans_final$damage <- log10(beans_final$damage)
+
+qqnorm(beans_final$damage)
+
+hist(beans_final$damage)
+
+##
+?aov
+model <- aov(damage ~ trt, beans_final)
+summary(model)
+shapiro.test(model$residuals)
+qqnorm(model$residuals)
+##
+
+
+##
+# if damage is not normal 
 kruskal.test(damage ~ trt, data = beans_final)
+##
 
 # vis of data with summary stats 
 group_by(beans_final, trt) %>% 
@@ -95,39 +119,15 @@ group_by(beans_final, trt) %>%
 library(ggpubr)
 ggboxplot(beans_final, x = 'trt', y = 'damage')
 
-
-##
-
-?shapiro.test
-shapiro.test(beans_final$damage)
-
-# beans_final$damage <- log10(beans_final$damage)
-
-qqnorm(beans_final$damage)
-
-hist(beans_final$damage)
-
-?aov
-model <- aov(damage ~ trt, beans_final)
-summary(model)
-shapiro.test(model$residuals)
-
-
-##
-
-#now I need to go back and get the total area
-# the first number is total, so I want to every other value, starting with 1 
-beans_total_test <- beans_test %>% 
-  select(leaf_plot, total_area) 
-# going to extract every odd row number
-rows <- nrow(beans_total_test)
-odd_rows <- seq_len(rows) %% 2
-beans_test_odd <- beans_total_test[odd_rows == 1, ]
-
-# all data odd extraction
-
-
-
+ggplot(beans_final, aes(trt, damage, fill = trt))+
+  geom_boxplot()+
+  stat_boxplot(geom = 'errorbar',
+               width = 0.2)+
+  theme_light()+
+  labs(title = 'Total Damage by Treatment')+
+  theme(legend.position = "none")+
+  scale_x_discrete(name = "Treatment")+
+  scale_y_continuous(name = "Total Damage (mm)")
 
 
 
